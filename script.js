bb9ef6ba424c5953e1b9e976780cfe508866b868
Daniel Sedaca <46464470+dsedaca@users.noreply.github.com>
2023-06-28 09:08:54 -0400

    Created single round function

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors"
+const rock = 'Rock';
+const paper = 'Paper';
+const scissors = 'Scissors';
+let playerChoice;
+let computerChoice;
 
 function getComputerChoice () {
 
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
-    let computerDecision;
+    let Choice = Math.floor(Math.random() * 3) + 1;
+    let Decision;
 
-    if (computerChoice === 1) {
-        computerDecision = rock;
-    } else if (computerChoice === 2) {
-        computerDecision = paper;
+    if (Choice === 1) {
+        Decision = rock;
+    } else if (Choice === 2) {
+        Decision = paper;
     } else {
-        computerDecision = scissors;
+        Decision = scissors;
     }
 
-    return computerDecision;
+    return Decision;
 }
 
-console.log(getComputerChoice());
+function playGame(player, computer) {
+    let outcome;
+    
+    if (player === computer) {
+        outcome = console.log('It\'s a Tie! You Both Chose ' + player + '!');
+    } else if ((player === 'Rock' && computer === 'Paper') || (player === 'Paper' && computer === 'Scissors') || (player === 'Scissors' && computer === 'Rock')) {
+        outcome = console.log('You Lose! ' + computer + ' beats ' + player + '!');
+    } else {
+        outcome = console.log('You Win! ' + player + ' beats ' + computer + '!');
+    }
+
+    return outcome;
+}
+
+computerChoice = getComputerChoice();
+
+playerChoice = prompt('Choose Rock, Paper, or Scissors!');
+
+playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+
+
+console.log(playGame(playerChoice, computerChoice));
